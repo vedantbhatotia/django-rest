94e32c6cc08c57a6c4cff593c8c49b6cf038209a
vedantbhatotia <vedantbhatotia@gmail.com>
2024-12-21 23:21:50 +0530

    basic api endpoint

---
+++ backend/api/views.py
@@ -0,0 +1,5 @@
+# from django.shortcuts import render
+# Create your views here.
+from django.http import JsonResponse
+def api_home(request, *args, **kwargs):
+    return JsonResponse({"message":"Hi there, this is your Django API response!"})
