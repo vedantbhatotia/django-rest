0f8949858858a8e0c2d93b1be1234e5fe6ac7712
vedantbhatotia <vedantbhatotia@gmail.com>
2024-12-26 03:16:53 +0530

    contdd

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,10 +5,11 @@ from products.models import Product
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from products.serializers import ProductSerializer
-@api_view(["GET"])
+# @api_view(])
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
-    instance = Product.objects.all().order_by("?").first()
-    data = {}
-    if instance:
-        data = ProductSerializer(instance).data
-    return Response(data)
+    serializer = ProductSerializer(data = request.data)
+    if serializer.is_valid():
+        data = serializer.save()
+        print(serializer.data)
+        return Response(serializer.data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,4 +1,4 @@
 import requests
-endpoint = "http://127.0.0.1:8000/api"
-get_response = requests.get(endpoint)
+endpoint = "http://127.0.0.1:8000/api/"
+get_response = requests.post(endpoint,json={"title":"hello world"})
 print(get_response.json())
